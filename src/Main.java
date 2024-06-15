6b4eced01f8fc1277b58189ede2fce8eeebb8cb9
EgorYunev <eg.yunev@gmail.com>
2024-06-16 00:02:20 +0300

    Little improvements

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,72 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("======== WELCOME TO YUNYA NOTIFICATIONS!========\n\nAll commands: \nhelp      add     see     close");
+        App.start();
+        new Thread(App::checkAnswer).start();
         new Thread(Alarm::alarmNotification).start();
-        Scanner scan = new Scanner(System.in);
-        close:
-        while (true) {
+    }
+}
+
+class App {
+    private static Scanner scan = new Scanner(System.in);
+    private static boolean appIsWork = true;
+    public static void start() {
+        System.out.println("======== WELCOME TO YUNYA NOTIFICATIONS!========\n\nAll commands: \nhelp      add     see     close     cancel");
+    }
+
+    public static boolean getAppIsWork() {
+        return appIsWork;
+    }
+
+    public static void checkAnswer() {
+        while (appIsWork) {
             String answer = scan.nextLine();
             switch (answer) {
                 case "add":
-                    System.out.println("Enter the name of notification: ");
-                    String name = scan.nextLine();
-                    System.out.println("Enter the date of new notification (Format: yyyy-MM-dd HH:mm): ");
-                    String strDate = scan.nextLine();
-                    LocalDateTime date = LocalDateTime.parse(strDate, Alarm.formatter);
-                    Alarm.getNotificationsArray().add(new Notifications(name, date));
+                    add();
                     break;
                 case "see":
-                    Alarm.seeAllNotifications();
+                    see();
                     break;
                 case "help":
-                    System.out.println("All commands: \nadd - Add a new reminder");
-                    System.out.println("see - Show all created reminders");
-                    System.out.println("close - Close the application");
+                    help();
                     break;
-                case "close" :
-                    System.out.println("GoodBye!");
-                    break close;
+                case "close":
+                    close();
+                    break;
+                default:
+                    System.out.println("Unknown command");
             }
         }
     }
+    private static void add() {
+        System.out.println("Enter the name of notification: ");
+        String name = scan.nextLine();
+        System.out.println("Enter the date of new notification (Format: yyyy-MM-dd HH:mm): ");
+        String strDate = scan.nextLine();
+        LocalDateTime current = LocalDateTime.now();
+        LocalDateTime date = LocalDateTime.parse(strDate, Alarm.formatter);
+        if (date.isBefore(current)) {
+            System.out.println("Incorrect date");
+            return;
+        }
+        Alarm.getNotificationsArray().add(new Notifications(name, date));
+        System.out.println("New notification created!");
+    }
+    private static void see() {
+        Alarm.seeAllNotifications();
+    }
+    private static void help() {
+        System.out.println("All commands: \nadd - Add a new reminder");
+        System.out.println("see - Show all created reminders");
+        System.out.println("close - Close the application");
+        System.out.println("cancel - Сancels the action");
+    }
+    private static void close() {
+        System.out.println("GoodBye!");
+        appIsWork = false;
+    }
+
 }
 
 class Notifications {
@@ -63,7 +100,7 @@ class Alarm {
     }
 
     public static void alarmNotification() {
-        while (true) {
+        while (App.getAppIsWork()) {
             LocalDateTime current = LocalDateTime.now();
             for (int i = 0; i < notificationsArray.size(); i++) {
                 if (notificationsArray.get(i).getNotificationTime().isBefore(current)) {
